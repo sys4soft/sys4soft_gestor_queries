6e95f06bd35324b294d3f9d2946dc2d76e2f51ef
João Ribeiro <business@sys4soft.com>
2023-08-13 07:59:30 -0700

    JR

--- app/Database/Seeds/QueriesSeeder.php
+++ app/Database/Seeds/QueriesSeeder.php
@@ -24,7 +24,71 @@ class QueriesSeeder extends Seeder
                 'project' => 'Project 1',
                 'query' => 'SELECT * FROM table2',
                 'created_at' => date('Y-m-d H:i:s')
-            ]
+            ],
+            [
+                'id_user' => 1,
+                'query_name' => 'Query 3',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 2',
+                'query' => 'SELECT * FROM table3',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 1,
+                'query_name' => 'Query 4',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 2',
+                'query' => 'SELECT * FROM table4',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 1,
+                'query_name' => 'Query 5',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 2',
+                'query' => 'SELECT * FROM table5',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 2,
+                'query_name' => 'Query 6',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 3',
+                'query' => 'SELECT * FROM table6',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 2,
+                'query_name' => 'Query 7',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 3',
+                'query' => 'SELECT * FROM table7',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 2,
+                'query_name' => 'Query 8',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 3',
+                'query' => 'SELECT * FROM table8',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 2,
+                'query_name' => 'Query 9',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 4',
+                'query' => 'SELECT * FROM table8',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'id_user' => 2,
+                'query_name' => 'Query 10',
+                'query_tags' => 'tag1, tag2, tag3',
+                'project' => 'Project 4',
+                'query' => 'SELECT * FROM table10',
+                'created_at' => date('Y-m-d H:i:s')
+            ],
         ];
 
         foreach ($data as $query) {
