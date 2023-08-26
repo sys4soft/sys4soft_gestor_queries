e8cb7a448aafa6fc8fe2eb5d195b753f9b8c18a8
João Ribeiro <business@sys4soft.com>
2023-08-26 15:21:29 -0700

    JR

--- app/Views/view_query.php
+++ app/Views/view_query.php
@@ -8,7 +8,7 @@
             <h4><strong><?= $query->query_name ?></strong></h4>
             <h5><?= $query->project ?></h5>
             <p class="opacity-50"> <?= $query->query_tags ?></p>
-            <textarea id="" rows="20" class="form-control">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam necessitatibus beatae cupiditate dicta autem numquam voluptas quae incidunt tempora iure.</textarea>
+            <textarea id="text_query" rows="20" class="form-control"><?= $query->query ?></textarea>
 
             <div class="row mt-2">
                 <div class="col">
@@ -17,13 +17,25 @@
                 <div class="col text-end">
                     <a href="#" class="btn btn btn-primary px-5">Eliminar</a>
                     <a href="#" class="btn btn btn-primary px-5">Editar</a>
-                    <a href="#" class="btn btn btn-secondary px-5">Copiar</a>
+                    <span id="copy" class="btn btn btn-secondary px-5">Copiar</span>
                 </div>
             </div>
         </div>
     </div>
 </section>
 
-
+<script>
+    document.querySelector('#copy').addEventListener('click', () => {
+        const textarea = document.querySelector('#text_query')
+        navigator.clipboard.writeText('').then(() => {
+            navigator.clipboard.writeText(textarea.value).then(() => {
+                document.querySelector('#query_copied').classList.remove('d-none')
+                setTimeout(() => {
+                    document.querySelector('#query_copied').classList.add('d-none')
+                }, 2000)
+            })
+        })
+    })
+</script>
 
 <?= $this->endSection() ?>
